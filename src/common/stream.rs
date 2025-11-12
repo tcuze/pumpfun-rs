4fc53e99cc7e284b9ab8034ab62245f5b867588e
tcuze <41377478+tcuze@users.noreply.github.com>
2025-11-12 16:48:21 +0800

    常见兼容之前的记录

--- src/common/stream.rs
+++ src/common/stream.rs
@@ -63,7 +63,7 @@ pub struct TradeEvent {
     pub total_claimed_tokens: u64,
     pub current_sol_volume: u64,
     pub last_update_timestamp: i64,
-    pub ix_name: String,
+    pub ix_name: Option<String>,
 }
 
 /// Event emitted when a bonding curve operation completes
